447889f583a197002d8828d034d1fd5320bf0372
KE9SH1N <K3NSHI9@gmail.com>
2022-12-29 18:09:50 +0600

    fundamentals of js initializing

---
+++ fundamentals.js
@@ -0,0 +1,44 @@
+// INTRODUCTION
+console.log("Hello World!");
+
+let js = "amazing";
+console.log (40+8+23-10);
+
+// VARIABLES
+let firstName = "Jonas";
+console.log(firstName);
+
+// DATA TYPES
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof "jonas");
+
+jsIsFun = "yes!";
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// VAR LET CONST
+
+let age = 30;  //can changeable in future
+age =32;
+
+const birthYear =  1994; //can't changeable in future
+
+// older version of let
+var job = "coder";
+job = "teacher";
+
+
+// OPERATORS
+
+const ageJonas = 2037 - 1991;
+console.log(ageJonas);
+
