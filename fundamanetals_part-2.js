5bd48f5cef4fafc3f9bbc346bbd6363df2b07f10
KE9SH1N <K3NSHI9@gmail.com>
2023-01-09 23:17:44 +0600

    added function

--- fundamanetals_part-2.js
+++ fundamanetals_part-2.js
@@ -5,5 +5,23 @@ let hasDriversLicense = false;
 
 const passTest = true;
 
-if(passTest) hasDriverLicense = true;
+if(passTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log('i can drive');
+
+
+// FUNCTIONS 
+
+function fruitProcessor(apples, oranges){
+    //console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 3);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(2, 4);
+console.log(orangeJuice);
+
+// FUNCTION DECLARATION VS EXPRESSION 
+
