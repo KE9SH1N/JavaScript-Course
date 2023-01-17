9263839373e1cd25dd898b5f14d628640e5e7bfb
KE9SH1N <K3NSHI9@gmail.com>
2023-01-17 22:35:11 +0600

    added arrow function

--- fundamanetals_part-2.js
+++ fundamanetals_part-2.js
@@ -41,3 +41,19 @@ const calcAge2 = function (birthYear){
 const age2 = calcAge2(1992);
 console.log(age1, age2);
 
+// ARROW FUNCTION 
+
+const calcAge3 = birthYear=> 2037-birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+const yearuntilRetirement = (birthYear, firstName)=>{
+    const age = 2037-birthYear;
+    const retirement = 65-age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearuntilRetirement(1991,"jonas"));
+console.log(yearuntilRetirement(1980,"bob"));
